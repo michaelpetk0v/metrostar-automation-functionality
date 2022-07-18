e24970a25356d1d48be69e2c21dd298c2f9d6af9
Mihail Petkov <michaelpetk0v@icloud.com>
2022-07-19 00:37:49 +0100

    metrostar automation

---
+++ metroStarAutomation/page_objects/expert.page.js
@@ -0,0 +1,22 @@
+exports.Expert = class Expert {
+
+    constructor(page) {
+        this.page = page;
+        this.cookiesBtn = page.locator('text=Accept');
+        this.aboutBtn = page.locator('[id="menu-item-181"] [class="menu-text"]');
+        this.companyBtn = page.locator('[id="menu-item-109"] [class="fusion-background-highlight"]');
+        this.whatWeDoBtn = page.locator('a:has-text("Explore What We Do")');
+        this.letsTalkBtn = page.locator('a:has-text("let’s talk")');
+        this.nameInput = page.locator('[id="firstname-2fb024fc-9e78-4f77-8c73-0df00e4a4cef"]');
+        this.emailInput = page.locator('[id="email-2fb024fc-9e78-4f77-8c73-0df00e4a4cef"]');
+        this.devSecOpsBtn = page.locator('[id="select_capabilities2-2fb024fc-9e78-4f77-8c73-0df00e4a4cef"]');
+        this.yourRequestField = page.locator('[id="multi_line_text_area-2fb024fc-9e78-4f77-8c73-0df00e4a4cef"]');
+        this.submitBtn = page.locator('.hs-submit [type="submit"]');
+    }
+    async submit(name, email) {
+        await this.nameInput.fill(name);
+        await this.emailInput.fill(email);
+        
+
+    }
+}
---
+++ metroStarAutomation/tests/expert.spec.js
@@ -0,0 +1,31 @@
+const { test, expect } = require('@playwright/test');
+const {Expert} = require('../page_objects/expert.page')
+const credentials = require('../data/credentials.json');
+let expert;
+
+// //Runs each test
+test.beforeEach(async ({ page }) => {
+  expert = new Expert(page);
+  await page.goto('https://www.metrostar.com/');
+});
+
+// Describe is equivalent of a test suite
+test.describe('contact an expert', () => {
+  
+  // Test is equivalent of a test case
+  test('user should be able to contact an expert', async ({ page }) => {
+    await expert.cookiesBtn.click();
+    await expert.aboutBtn.click();
+    await expert.companyBtn.click();
+    await expert.whatWeDoBtn.first().click();
+    await expect(page).toHaveURL('https://www.metrostar.com/services/');
+    await expert.letsTalkBtn.first().click();
+    await expect(page).toHaveURL('https://www.metrostar.com/capabilities-contact/');
+    await expert.submit(credentials.contactanexpert.name, credentials.contactanexpert.email);
+    await expert.devSecOpsBtn.click();
+    await expert.yourRequestField.fill("Hi there. It's nice to see you again.");
+    await expert.submitBtn.click();
+    
+  });
+
+});
